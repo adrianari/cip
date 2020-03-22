ab9223886c5705c61601593e9ec3cf47e9b5e59f
adritschka <adrianaricklin@gmail.com>
2020-03-22 21:51:01 +0100

    Tets zu Douglas_CH

---
+++ test.py
@@ -0,0 +1,71 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import time
+import csv
+
+
+class Product():
+    def __init__(self, title, brand_line, price, base_price): #, image):
+        self.title = title
+        self.brand_line = brand_line
+        self.price = price
+        self.base_price = base_price
+        #self.image = image
+
+class ProductFetcher():
+    def fetch(self):
+        homepages = []
+        url = "https://www.douglas.ch/de/c/parfum/damenduefte/damenparfum/010106?page=1"
+        articles = []
+
+        while url not in homepages:
+            homepages.append(url)
+            time.sleep(1)
+            r = requests.get(url)
+            doc = BeautifulSoup(r.text, "html.parser")
+
+            for product in doc.select(".product-tile"):
+                title = product.select_one(".product-tile__top-brand").text
+                brand_line = product.select_one(".product-tile__brand-line").text
+                #category = product.select_one(".product-tile__category").text
+                price = product.select_one(".product-price__no-discount")
+                if price:
+                    price = price.text
+                else:
+                    continue
+                base_price = product.select_one(".product-price__base-price").text
+                #image = product.select_one("img").attrs["src"]
+
+
+                crawled = Product(title, brand_line, price, base_price) #, image)
+                articles.append(crawled)
+
+            for x in doc.find_all("a", class_="link link--text pagination__arrow active"):
+                y = x.get("href")
+                y = urljoin(url,y)
+                if y not in homepages:
+                    next = urljoin(url, y)
+                    url = next
+                else:
+                    print("stuck in a loop")
+                    break
+
+
+            print(homepages)
+        return articles
+
+
+
+
+
+#for article in articles:
+#    print(article.title)
+fetcher = ProductFetcher()
+articles = fetcher.fetch()
+
+with open( 'douglas.ch.csv', 'w', newline='', encoding= "utf-8" ) as csvfile:
+    blogwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for article in fetcher.fetch():
+        blogwriter.writerow( [article.title, article.brand_line, article.price, article.base_price] )
