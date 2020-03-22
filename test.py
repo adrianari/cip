f500318489ab449193be8936065ac33e7e681a78
adritschka <adrianaricklin@gmail.com>
2020-03-22 23:17:35 +0100

    with the right crawling code

--- test.py
+++ test.py
@@ -13,43 +13,37 @@ class Product():
         self.base_price = base_price
         #self.image = image
 
+
 class ProductFetcher():
     def fetch(self):
-        homepages = []
         url = "https://www.douglas.ch/de/c/parfum/damenduefte/damenparfum/010106?page=1"
+
         articles = []
+        time.sleep(1)
+        r = requests.get(url)
+        doc = BeautifulSoup(r.text, "html.parser")
 
-        while url not in homepages:
-            homepages.append(url)
-            time.sleep(1)
-            r = requests.get(url)
-            doc = BeautifulSoup(r.text, "html.parser")
+        homepages = []
+        for x in doc.find_all("a", class_ = "link link--no-decoration pagination-title__option-link active"):
+            homepages.append(x.get("href"))
 
-            for product in doc.select(".product-tile"):
-                title = product.select_one(".product-tile__top-brand").text
-                brand_line = product.select_one(".product-tile__brand-line").text
-                #category = product.select_one(".product-tile__category").text
-                price = product.select_one(".product-price__no-discount")
-                if price:
-                    price = price.text
-                else:
-                    continue
-                base_price = product.select_one(".product-price__base-price").text
-                #image = product.select_one("img").attrs["src"]
-
-
-                crawled = Product(title, brand_line, price, base_price) #, image)
-                articles.append(crawled)
-
-            for x in doc.find_all("a", class_="link link--text pagination__arrow active"):
-                if urljoin(url,x.get("href")) not in homepages:
-                    url = urljoin(url,x.get("href"))
-                    break
-                else:
-                    pass
+        for product in doc.select(".product-tile"):
+            title = product.select_one(".product-tile__top-brand").text
+            brand_line = product.select_one(".product-tile__brand-line").text
+            #category = product.select_one(".product-tile__category").text
+            price = product.select_one(".product-price__no-discount")
+            if price:
+                price = price.text
+            else:
+                continue
+            base_price = product.select_one(".product-price__base-price").text
+            #image = product.select_one("img").attrs["src"]
 
 
-            print(homepages)
+            crawled = Product(title, brand_line, price, base_price) #, image)
+            articles.append(crawled)
+
+
         return articles
 
 
