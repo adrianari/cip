e455ad129275719b72d387b93dce2d3aa3fa1a2e
adritschka <adrianaricklin@gmail.com>
2020-04-03 16:37:15 +0200

    pandas

--- we_do_a_panda.py
+++ we_do_a_panda.py
@@ -5,23 +5,13 @@ import csv
 def calculator(df):
 
     data = pd.read_csv(df, sep=";")
+
     df = pd.DataFrame(data, columns=["Title", "Brand", "Kategorie", "ml", "Preis"])
-    spalte = []
-
-    for line in df:
-        df["Preis"] = pd.to_numeric(df["Preis"])
-        df["ml"] = pd.to_numeric(df["ml"])
-        df["Preis pro 100 ml"] = df["Preis"] / df["ml"] * 100
-
-    print(spalte)
 
 
-
-
-
-
-
-
+    df["Preis"] = pd.to_numeric(df["Preis"])
+    df["ml"] = pd.to_numeric(df["ml"])
+    df["Preis pro 100 ml"] = df["Preis"] / df["ml"] * 100
 
 
 
