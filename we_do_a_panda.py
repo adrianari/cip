e1e216e960996289be86a8ddc4d0a2fe03457474
adritschka <adrianaricklin@gmail.com>
2020-04-10 17:15:09 +0200

    reload

--- we_do_a_panda.py
+++ we_do_a_panda.py
@@ -8,7 +8,10 @@ def calculator(data):
 
     #Cleaning
     df.iloc[58] = df.iloc[58].replace(to_replace = 10, value=60)
-    df.iloc[58] = df.iloc[58].replace(to_replace = "The Only One  50 ml & Mini", value="The Only One")
+    df.iloc[58] = df.iloc[58].replace(to_replace = "The Only One  50 ml & Mini", value="The Only One Set")
+
+    #Delete multiple's (first 24 parfums due to setting up of pages crawling)
+    df = df.drop([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23])
 
     #Calculating price for 100ml
     df["Preis"] = pd.to_numeric(df["Preis"])
